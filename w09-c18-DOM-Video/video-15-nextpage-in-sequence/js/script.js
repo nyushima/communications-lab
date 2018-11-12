e748d45f710bc2081faab488ad4c1e727254bb9b
MOQN <moonjunghyun@gmail.com>
2018-11-12 23:36:46 +0800

    added more HTML video examples

---
+++ w09-c18-DOM-Video/video-15-nextpage-in-sequence/js/script.js
@@ -0,0 +1,83 @@
+var vid = document.getElementById('myVideo');
+var btnReplay = document.getElementById('btnReplay');
+var btnStop = document.getElementById('btnStop');
+
+btnReplay.style.display = "block"; // shown
+btnReplay.style.opacity = "0.3";
+btnStop.style.display = "none"; // hidden
+btnStop.style.opacity = "0.3";
+
+
+var step = 0;
+
+function timeUpdated() {
+  console.log(vid.currentTime);
+
+  // update the "step" variable first
+  var t = vid.currentTime;
+  if (t >= 0 && t < 2.5) {
+    step = 0;
+  }
+  else if (t >= 2.5 && t < 4.5) {
+    step = 1;
+  }
+  else if (t >= 4.5 && t < 5.0) {
+    step = 2;
+  }
+  else {
+    step = 3;
+  }
+
+  // then use it
+  if (step == 1) {
+    btnStop.style.display = "block"; // shown
+  } else {
+    btnStop.style.display = "none"; // hidden
+  }
+  if (step == 3) {
+    window.open("nextpage.html", "_self", "");
+    //window.open("url", "target", "specs"); - check the slides.
+  }
+}
+
+
+window.onload = function() {
+  adjustVideoSize();
+}
+
+window.onresize = function() {
+  adjustVideoSize();
+}
+
+function adjustVideoSize() {
+  // check the dementions of the video clip
+  // 4:3? 16:9?
+  if (window.innerWidth/window.innerHeight >= 16/9) {
+    vid.width = window.innerWidth;
+    vid.height = window.innerWidth * 9/16;
+  } else {
+    vid.width = window.innerHeight * 16/9;
+    vid.height = window.innerHeight;
+  }
+  // And then, change the button's position and size according to the video's width and height;
+  btnReplay.style.position = "absolute";
+  btnReplay.style.width = Math.floor(vid.width * 0.10) + "px";
+  btnReplay.style.height = Math.floor(vid.height * 0.10) + "px";
+  btnReplay.style.left = Math.floor(vid.width * 0.15) + "px";
+  btnReplay.style.top = Math.floor(vid.height * 0.15) + "px";
+
+  btnStop.style.position = "absolute";
+  btnStop.style.width = Math.floor(vid.width * 0.10) + "px";
+  btnStop.style.height = Math.floor(vid.height * 0.10) + "px";
+  btnStop.style.left = Math.floor(vid.width * 0.30) + "px";
+  btnStop.style.top = Math.floor(vid.height * 0.15) + "px";
+}
+
+function replayVideo() {
+  vid.currentTime = 0;
+  vid.play();
+}
+
+function pauseVideo() {
+  vid.pause();
+}
